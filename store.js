fbed3a5ac523a9f10c8e7e20c0be5f5923efe1d0
Lezajic Petar <lezajicpetar@gmail.com>
2025-05-01 20:50:55 +0200

    Napravljena petlja koja puni tabelu podatcima i renderovana tabela

--- store.js
+++ store.js
@@ -16,3 +16,41 @@ let artikal4 = new Aritakl(4, "nimbus 2000", 1000000, true)
 
 let listaArtikala = [artikal1, artikal2, artikal3, artikal4]
 
+let table = document.querySelector("#artikli")
+
+
+    for (let artikal of listaArtikala) {
+        let tr = document.createElement("tr")
+    
+        let id = document.createElement("td")
+        let naziv = document.createElement("td")
+        let cena = document.createElement("td")
+        let dostupnost = document.createElement("td")
+        
+        
+
+        id.textContent = artikal.id
+        naziv.textContent = artikal.naziv
+        cena.textContent = artikal.cena
+        if (artikal.dostupnost) {
+            dostupnost.textContent = "DA"
+        }
+        else {
+            dostupnost.textContent = "NE"
+            tr.style.backgroundColor = "rgba(255, 0, 0, 0.3)"
+            id.style.borderColor = "rgba(255, 0, 0, 0.5)"
+            naziv.style.borderColor = "rgba(255, 0, 0, 0.5)"
+            cena.style.borderColor = "rgba(255, 0, 0, 0.5)"
+            dostupnost.style.borderColor = "rgba(255, 0, 0, 0.5)"
+
+        }
+        
+
+    
+        tr.appendChild(id)
+        tr.appendChild(naziv)
+        tr.appendChild(cena)
+        tr.appendChild(dostupnost)
+    
+        table.appendChild(tr)
+    }
